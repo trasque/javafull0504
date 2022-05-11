d51dca7d5b16f4056219427342bc765cb56d9145
trasque <taisajazz@gmail.com>
2022-05-12 00:44:38 +0900

    Remove non use imports.

--- src/main/java/com/raisetech/javafull0504/controller/PlayerController.java
+++ src/main/java/com/raisetech/javafull0504/controller/PlayerController.java
@@ -1,13 +1,10 @@
 package com.raisetech.javafull0504.controller;
 
-import java.util.List;
-
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.raisetech.javafull0504.mapper.PlayerMapper;
-import com.raisetech.javafull0504.entity.Player;
 
 
 // @RestController はJSONやXMLなどのAPI的なデータを返すようになる
